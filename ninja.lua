4ab2566ab8a5fb2964df7ef1631fc6b39f59d254
Marcus Schappi <marcus@schappi.com>
2013-01-16 14:09:25 +1100

    added ninja

--- ninja.lua
+++ ninja.lua
@@ -1,11 +1,15 @@
-function update_ninja (token, device, data)
+-- function update_ninja (token, device, data)
+-- 
+--   local ninja = http.request({
+--   	method='put',
+--   	url='https://api.ninja.is/rest/v0/device/' .. device .. '?user_access_token=' .. token,
+--   	data = '{ "DA" : "'.. data ..'" }',
+--     headers = { ['Content-Type'] = 'application/json' }
+--   	}).statuscode
+--   end
+-- 
+-- end
 
-  local ninja = http.request({
-  	method='put',
-  	url='https://api.ninja.is/rest/v0/device/' .. device .. '?user_access_token=' .. token,
-  	data = '{ "DA" : "'.. data ..'" }',
-    headers = { ['Content-Type'] = 'application/json' }
-  	}).statuscode
-  end
-
+function update_ninja (x, y, z)
+  return "something"
 end
